244dfc1541ff4b6a39a0657e2165083c09f8153e
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2012-08-23 09:14:53 +0200

    Laden 2

---
+++ QuizTests/QuizTests.h
@@ -0,0 +1,15 @@
+//
+//  QuizTests.h
+//  QuizTests
+//
+//  Created by Ruedi Heimlicher on 05.August.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QuizTests : SenTestCase
+{
+   }
+
+@end
---
+++ QuizTests/QuizTests.m
@@ -0,0 +1,32 @@
+//
+//  QuizTests.m
+//  QuizTests
+//
+//  Created by Ruedi Heimlicher on 05.August.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "QuizTests.h"
+
+@implementation QuizTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in QuizTests");
+}
+
+@end
